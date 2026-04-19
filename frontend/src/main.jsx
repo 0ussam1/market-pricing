1e5eeeab9cac213bd3338ed13559adfd8a230319
Amine Ait Laamim <amineaitlaamim16@gmail.com>
2026-04-19 16:51:01 +0200

    fix frontend using docker

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>Market Research & Pricing Analysis</h1>
+    <p>Frontend is running with Vite!</p>
+  </React.StrictMode>,
+)
